a9c2d138e07f166219d8da98c31e5fcf7ec7dc71
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-08-28 03:32:37 +0200

    g2: releasetools: Powered by HH
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -53,6 +53,9 @@ def FullOTA_InstallEnd(info):
   info.script.script = [cmd for cmd in info.script.script if not "show_progress(0.100000, 0);" in cmd]
   info.script.AppendExtra('package_extract_file("boot.img", "/tmp/boot.img");')
   info.script.AppendExtra('ifelse(is_mounted("/system"), unmount("/system"));')
+  info.script.Print(" ");
+  info.script.Print("Powered by HealthyHoney KERNEL");
+  info.script.Print(" ");
   info.script.Mount("/system")
   info.script.AppendExtra('assert(run_program("/system/bin/panel.sh") == 0);')
   info.script.AppendExtra('assert(run_program("/sbin/sh", "-c", "busybox dd if=/tmp/boot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot") == 0);')
