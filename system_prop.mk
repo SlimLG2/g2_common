b6cebb1deb41f4996836d0d85bffa7ccc2510351
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-09-09 22:53:29 +0200

    g2: Move memory + hardware UI settings and update dalvik heap

--- system_prop.mk
+++ system_prop.mk
@@ -155,3 +155,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # I/O scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.io.scheduler=zen
+
+# Memory
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.25 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
