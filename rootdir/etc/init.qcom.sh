b6fb1d6e054f8d99bff153865fb833123f6d3b04
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-09-12 11:20:41 +0200

    g2: init: qcom: Setup persist sensors debug levels

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2009-2014, The Linux Foundation. All rights reserved.
+# Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -25,6 +25,9 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+setprop persist.debug.sensors.hal e
+setprop persist.debug.ar.hal e
 
 target=`getprop ro.board.platform`
 if [ -f /sys/devices/soc0/soc_id ]; then
