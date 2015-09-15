721eab7095390709ca371a1787f486f5f9c0e300
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-09-15 14:31:40 +0200

    Revert "g2: power: Decrease app launch boost"
    This reverts commit 3282838ca99571f7355fb52b35ad8b62e0c208ed.

--- power/power-8974.c
+++ power/power-8974.c
@@ -118,7 +118,9 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
 
     if (hint == POWER_HINT_LAUNCH_BOOST) {
         int duration = 2000;
-        int resources[] = { CPUS_ONLINE_MIN_3, 0x211, 0x311, 0x411 };
+        int resources[] = { CPUS_ONLINE_MIN_3,
+            CPU0_MIN_FREQ_TURBO_MAX, CPU1_MIN_FREQ_TURBO_MAX,
+            CPU2_MIN_FREQ_TURBO_MAX, CPU3_MIN_FREQ_TURBO_MAX };
 
         interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
 
