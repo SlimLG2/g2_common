d19485702dd9dde91eaa40ef4bdf8ff4a49f2311
Ethan Chen <intervigil@gmail.com>
2015-10-04 20:44:43 -0700

    g2-common: Introduce init.recovery.g2.rc
    Change-Id: I75296a8a89444ee1cfa0f83819183901d121d686
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
    
    Conflicts:
    	rootdir/Android.mk
    	rootdir/etc/init.g2.rc

--- g2.mk
+++ g2.mk
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
     init.g2.rc \
     init.g2.power.rc \
     init.g2.usb.rc \
+    init.recovery.g2.rc \
     panel.sh \
     set_baseband.sh \
     ueventd.g2.rc
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,6 +28,14 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.g2.power.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.g2.power.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.g2.usb.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
@@ -36,11 +44,11 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.g2.power.rc
+LOCAL_MODULE            := init.recovery.g2.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.power.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
