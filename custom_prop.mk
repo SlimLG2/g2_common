b636e2d68bd6131bba83b3e0f2de3a05c8ae3362
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-10-18 16:50:30 +0200

    g2-common: custom_prop: Include LIBARGS in RIL

--- custom_prop.mk
+++ custom_prop.mk
@@ -19,6 +19,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wlan.chip.vendor=brcm \
     wlan.chip.version=bcm4335
 
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libargs=-d /dev/smd0
+
 #
 # Audio/Video
 #
